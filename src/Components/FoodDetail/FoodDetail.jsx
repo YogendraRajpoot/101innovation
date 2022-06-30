655afba57752f41274a7a1d6197ef7bb8ba5e5c1
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-07-01 01:21:56 +0530

    complete

--- src/Components/FoodDetail/FoodDetail.jsx
+++ src/Components/FoodDetail/FoodDetail.jsx
@@ -12,22 +12,31 @@ const PopUpModal = styled.div`
   height: 90vh;
   margin-left: auto;
   margin-right: auto;
-  overflow: scroll;
+  // overflow: scroll;
   .buttonDiv {
     background: transparent;
     border: 2px solid black;
-    padding: 5vh;
+    padding: 4vh;
   }
   .tableDiv {
     padding: 2%;
   }
 `;
 
-const TableWrapper = styled.table`
+const TableWrapper = styled.div`
   border: 2px solid;
   background: transparent;
   width: 100%;
+  height: 76vh;
+  overflow: scroll;
+  table {
+    width: 100%;
+  }
   td {
+    font-weight: 600;
+    font-size: 1vw;
+    // margin-top: 5%;
+    height: 5vh;
     width: 50%;
     border: 2px solid;
     text-align: center;
@@ -58,26 +67,30 @@ export const FoodDetail = (props) => {
       </div>
       <div className="tableDiv">
         <TableWrapper>
-          {/* <thead>
+          <table>
+            {/* <thead>
             <tr>
               <th>one</th>
               <th>one</th>
-            </tr>
-          </thead> */}
-          <tbody>
-            {Object.entries(data).map(([key, values]) => (
-              <tr>
-                <td>{key}</td>
-                {key === "url" ? (
-                  <td>
-                    <a href={values}>Link</a>
-                  </td>
-                ) : (
-                  <td>{values}</td>
-                )}
               </tr>
-            ))}
-          </tbody>
+            </thead> */}
+            <tbody>
+              {Object.entries(data).map(([key, values]) => (
+                <tr>
+                  <td>{key.toUpperCase()}</td>
+                  {key === "url" ? (
+                    <td>
+                      <a href={values}>Link</a>
+                    </td>
+                  ) : values === null ? (
+                    <td>Null</td>
+                  ) : (
+                    <td>{values}</td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </TableWrapper>
       </div>
     </PopUpModal>
